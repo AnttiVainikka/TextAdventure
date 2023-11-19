b47553c34052e8ba05117e8b0bc225a292837db8
Csuka Szabolcs <av80sh@INF.ELTE.HU>
2023-11-19 20:03:27 +0200

    Summarization based on number of words

--- src/Generation/Descriptions/summarize_context/model.py
+++ src/Generation/Descriptions/summarize_context/model.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 
-MAX_LENGTH = 300
+MAX_LENGTH = 50
 
 class Model(BaseModel):
-    summarized_context: str = Field(title=f'The summarized context at most {MAX_LENGTH} characters', max_length=MAX_LENGTH)
+    summarized_context: str = Field(title=f'The summarized context at most {MAX_LENGTH} words')
--- src/Generation/summarize_context.py
+++ src/Generation/summarize_context.py
@@ -2,5 +2,5 @@ from Generation.generator import llm_create
 from Generation.Descriptions.summarize_context.model import MAX_LENGTH
 
 def summarize_context(context: str) -> str:
-    if len(context) <= MAX_LENGTH: return context
-    return llm_create('summarize_context', context=context, number_of_characters=MAX_LENGTH)[0].summarized_context
+    if len(context.split()) <= MAX_LENGTH: return context
+    return llm_create('summarize_context', context=context, number_of_words=MAX_LENGTH)[0].summarized_context
